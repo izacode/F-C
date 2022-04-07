32151bb23fb88f5ae07dac6d4d78173223ba122e
unknown <izaelec@gmail.com>
2022-04-07 09:13:44 -0400

    new katas added

--- codewars.js
+++ codewars.js
@@ -21,3 +21,23 @@
 //   }
 //   return years;
 // }
+
+// 3) Flatten and sort an array========================================================
+
+// function flattenAndSort(array) {
+//   return array.reduce((acc, curr) => acc.concat(curr),[]).sort((a,b)=>a-b);
+// }
+
+
+// 4) Make a function that does arithmetic!=============================================
+
+// function arithmetic(a, b, operator) {
+//   const operations = {
+//     add: (a, b) => a + b,
+//     subtract: (a, b) => a - b,
+//     multiply: (a, b) => a * b,
+//     divide: (a, b) => a / b,
+//   };
+
+//   return operations[operator](a, b);
+// }
