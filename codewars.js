2da1b3250d13d7e90f27f63038a33c3260068f51
unknown <izaelec@gmail.com>
2022-04-09 10:45:14 -0400

    7-8 added

--- codewars.js
+++ codewars.js
@@ -1,4 +1,4 @@
-// 1) Descending Order================================================================
+// 1) Descending Order==================================================================================================================================================
 
 // function descendingOrder(n) {
 //   //...
@@ -9,7 +9,7 @@
 //     .join("");
 // }
 
-// 2) Money, Money, Money==============================================================
+// 2) Money, Money, Money=========================================================================================================================================
 
 // function calculateYears(principal, interest, tax, desired) {
 //   let years = 0;
@@ -22,15 +22,14 @@
 //   return years;
 // }
 
-// 3) Flatten and sort an array========================================================
+// 3) Flatten and sort an array===================================================================================================================================
 
 // function flattenAndSort(array) {
 //   return array.reduce((acc, curr) => acc.concat(curr),[]).sort((a,b)=>a-b);
 // }
 
 
-// 4) Make a function that does arithmetic!=============================================
-
+// 4) Make a function that does arithmetic!! =====================================================================================================================
 // function arithmetic(a, b, operator) {
 //   const operations = {
 //     add: (a, b) => a + b,
@@ -41,3 +40,65 @@
 
 //   return operations[operator](a, b);
 // }
+
+
+
+
+// 5) Anagram Detection===! =====================================================================================================================================
+// var isAnagram = function(test, original) {
+// return test.toLowerCase().split('').sort().join()===original.toLowerCase().split('').sort().join()
+// };
+
+// 6) The highest profit wins! ===================================================================================================================================
+
+// function minMax(arr) {
+//   const sortedArr = arr.sort((a, b) => a - b);
+//   const min = sortedArr[0];
+//   const max = sortedArr[sortedArr.length - 1];
+
+//   return [min, max];
+// }
+
+
+
+// 7) Printer Errors        ===================================================================================================================================
+
+// function printerError(s) {
+   
+//   let numerator = 0;
+//   let denominator = 0;
+  
+//   s.split('').forEach(l=>{
+//     denominator++
+//     if(!l.match(/[a-m]/)){
+//       numerator ++
+//     }
+//   })
+//   return `${numerator}/${denominator}`
+// }
+
+// const printerError = (s) => `${s.replace(/[a-m]/gi, "").length}/${s.length}`; to check replace
+
+
+
+
+
+
+// 8) Sum of a sequence        ===================================================================================================================================
+// Mine
+// const sequenceSum = (begin, end, step) => {
+//   if (begin > end) return 0;
+//   let sum = 0;
+//   for (let num = begin; num <= end; num += step) {
+//     sum += num;
+//   }
+//   return sum;
+// };
+// Best
+// const sequenceSum = (begin, end, step) => {
+//   if (begin > end) {
+//     return 0;
+//   }
+//   return begin + sequenceSum(begin + step, end, step);
+// };
+// or   sequenceSum = (b, e, s) => (b > e ? 0 : b + sequenceSum(b + s, e, s));
